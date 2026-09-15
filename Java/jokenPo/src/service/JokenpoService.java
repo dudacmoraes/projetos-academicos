e4cb1ad08166e280cc8ad9fa5cbb158672f71781
Maria Eduarda Costa <dudamoraes1709@gmail.com>
2026-09-15 09:34:52 -0300

    Fix output formatting and improve victory messages

--- Java/jokenPo/src/service/JokenpoService.java
+++ Java/jokenPo/src/service/JokenpoService.java
@@ -3,41 +3,34 @@ import model.Jogada;
 import model.Jogador;
 
 public class JokenpoService {
-
     public void analisarResultado(Jogador jogador1, Jogador jogador2) {
         if (jogador1.getJogada() == jogador2.getJogada()) {
-            System.out.print("Empate!\n");
+            System.out.println("Empate!");
+        }
+        else if (jogador1.getJogada() == Jogada.PEDRA && jogador2.getJogada() == Jogada.PAPEL) {
+            System.out.printf("%s venceu! PAPEL ganha de PEDRA.\n", jogador2.getNome());
+            jogador2.contabilizarVitorias();
+        }
+        else if (jogador1.getJogada() == Jogada.PEDRA && jogador2.getJogada() == Jogada.TESOURA) {
+            System.out.printf("%s venceu! PEDRA ganha de TESOURA.\n", jogador1.getNome());
+            jogador1.contabilizarVitorias();
         }
         else if (jogador1.getJogada() == Jogada.PAPEL && jogador2.getJogada() == Jogada.PEDRA) {
             System.out.printf("%s venceu! PAPEL ganha de PEDRA.\n", jogador1.getNome());
             jogador1.contabilizarVitorias();
         }
-
         else if (jogador1.getJogada() == Jogada.PAPEL && jogador2.getJogada() == Jogada.TESOURA) {
             System.out.printf("%s venceu! TESOURA ganha de PAPEL.\n", jogador2.getNome());
             jogador2.contabilizarVitorias();
         }
-
-        else if (jogador1.getJogada() == Jogada.PEDRA && jogador2.getJogada() == Jogada.PAPEL) {
-            System.out.printf("%s venceu! PAPEL ganha de PEDRA.\n", jogador2.getNome());
-            jogador2.contabilizarVitorias();
-        }
-
-        else if (jogador1.getJogada() == Jogada.PEDRA && jogador2.getJogada() == Jogada.TESOURA) {
-            System.out.printf("%s venceu! PEDRA ganha de TESOURA.\n", jogador1.getNome());
-            jogador1.contabilizarVitorias();
-        }
-
         else if (jogador1.getJogada() == Jogada.TESOURA && jogador2.getJogada() == Jogada.PEDRA) {
             System.out.printf("%s venceu! PEDRA ganha de TESOURA.\n", jogador2.getNome());
             jogador2.contabilizarVitorias();
         }
-
         else if (jogador1.getJogada() == Jogada.TESOURA && jogador2.getJogada() == Jogada.PAPEL) {
             System.out.printf("%s venceu! TESOURA ganha de PAPEL.\n", jogador1.getNome());
             jogador1.contabilizarVitorias();
-        }
-        else {
+        } else {
             System.out.println(" ");
         }
     }
@@ -50,13 +43,13 @@ public class JokenpoService {
 
     public void verificarCampeao(Jogador jogador1, Jogador jogador2) {
         System.out.println("-".repeat(30));
-
         if (jogador1.getVitorias() > jogador2.getVitorias()) {
-            System.out.printf("Vitória de %s", jogador1.getNome());
+            System.out.printf("Vitória de %s\n", jogador1.getNome());
         } else if (jogador2.getVitorias() > jogador1.getVitorias()) {
-            System.out.printf("Vitória de %s", jogador2.getNome());
+            System.out.printf("Vitória de %s\n", jogador2.getNome());
         } else {
             System.out.println("Empate! Ninguém venceu.");
         }
+        System.out.print("-".repeat(30));
     }
 }
