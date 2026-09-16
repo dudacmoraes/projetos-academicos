c6b2f22e14e16768a9be80ae212968cc315498ba
Maria Eduarda Costa <dudamoraes1709@gmail.com>
2026-09-16 11:36:19 -0300

    Create Campeao class with attributes and methods
    Implement Campeao class with attributes and methods for a champion character.

---
+++ Java/BatalhaCampeoes/src/model/Campeao.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Campeao {
+    private final int identificadorCampeao;
+    private final String nomeCampeao;
+    private int vida;
+    private final int ataque;
+    private final int defesa;
+
+    // CONSTRUTOR!!!!!!!!!! (Tem que ter TODOS os atributos da classe)
+    public Campeao(int identificadorCampeao, String nomeCampeao, int vida, int ataque, int defesa) {
+        this.identificadorCampeao = identificadorCampeao;
+        this.nomeCampeao = nomeCampeao;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d | %s | Vida: %d | Ataque: %d | Defesa: %d", identificadorCampeao, nomeCampeao, vida, ataque, defesa);
+    }
+
+    public int getIdentificadorCampeao() {
+        return identificadorCampeao;
+    }
+
+    public String getNomeCampeao() {
+        return nomeCampeao;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(double ganhoVida) {
+        vida = (int) (vida + (vida * ganhoVida));
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public void defender(int ataqueInimigo) {
+        int dano = ataqueInimigo - defesa;
+        vida -= dano;
+    }
+}
